e09086593828759e740b2ca37d4246c8e95f3ff3
anastarrr <92821335+anastarrr@users.noreply.github.com>
2025-06-08 20:41:34 +0300

    Deleted proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,13 +10,13 @@ export default defineConfig({
     }
   },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path
-      }
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3001',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path
+    //   }
+    // },
   },
   build: {
     outDir: '../server/public',
